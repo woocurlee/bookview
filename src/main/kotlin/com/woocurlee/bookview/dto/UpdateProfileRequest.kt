11dfcc53e885fc0e9940ae742177b61c442aec21
sorious77 <sorious77@naver.com>
2026-03-25 21:55:18 +0900

    :sparkles: BKVW-21 : 닉네임 변경 에러 처리

--- src/main/kotlin/com/woocurlee/bookview/dto/UpdateProfileRequest.kt
+++ src/main/kotlin/com/woocurlee/bookview/dto/UpdateProfileRequest.kt
@@ -2,6 +2,5 @@ package com.woocurlee.bookview.dto
 
 data class UpdateProfileRequest(
     val nickname: String,
-    val agreedToTerms: Boolean = false,
-    val termsVersion: String = "1.0",
+    val agreedToTerms: Boolean? = null,
 )
